3c07d57c5c5aab4665200afbd28785c59f714401
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-15 18:31:02 -0300

    chore(deps): add fastify dependency and update server setup - Added `fastify` as a dependency in `package.json`. - Updated `src/http/server.ts` to initialize a Fastify server. - Updated `yarn.lock` to reflect the new dependency.

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,10 @@
-console.log("hello world")
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.listen({ port: 3000 }).then(() => {
+  console.log(`
+    Server listening on port 3000
+    acesse em: http://localhost:3000
+  `)
+})
