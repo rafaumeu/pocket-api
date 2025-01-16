326247ae7caf5a97587d35718f4f7d246078f0a8
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-16 07:08:56 -0300

    feat(goals): add pending goals and goal completion functionality - Updated `createGoal` function in `src/functions/create-goal.ts` to improve code formatting and consistency. - Added `getWeekPendingGoals` and `createGoalCompletion` functions to `src/functions/` directory for retrieving pending goals and creating goal completions. - Implemented `getWeekPendingGoals` and `createGoalCompletion` functions to handle respective business logic. - Added `/pending-goals` and `/goal-completions` routes to `src/http/server.ts` to handle pending goals and goal completion requests.

--- src/functions/create-goal.ts
+++ src/functions/create-goal.ts
@@ -1,19 +1,25 @@
 import { db } from '../db'
 import { goals } from '../db/schema'
 
-interface CreadeGoalRequest {
-  title: string,
+interface CreateGoalRequest {
+  title: string
   desiredWeeklyFrequency: number
 }
-export async function createGoal({title, desiredWeeklyFrequency}: CreadeGoalRequest) {
-  const result = await db.insert(goals).values({
-    title,
-    desiredWeeklyFrequency
-  }).returning()
+export async function createGoal({
+  title,
+  desiredWeeklyFrequency,
+}: CreateGoalRequest) {
+  const result = await db
+    .insert(goals)
+    .values({
+      title,
+      desiredWeeklyFrequency,
+    })
+    .returning()
 
   const goal = result[0]
 
   return {
-    goal
+    goal,
   }
 }
--- src/http/server.ts
+++ src/http/server.ts
@@ -7,11 +7,32 @@ import {
 } from 'fastify-type-provider-zod'
 import z from 'zod'
 import { createGoal } from '../functions/create-goal'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
+import { createGoalCompletion } from '../functions/create-goal-completion'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
-
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+  return { pendingGoals }
+})
+app.post(
+  '/goal-completions',
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async (request, reply) => {
+    const { goalId } = request.body
+    await createGoalCompletion({
+      goalId,
+    })
+  }
+)
 app.post(
   '/goals',
   {
@@ -23,10 +44,10 @@ app.post(
     },
   },
   async (request, reply) => {
-    const {title, desiredWeeklyFrequency} = request.body
+    const { title, desiredWeeklyFrequency } = request.body
     await createGoal({
       title,
-      desiredWeeklyFrequency
+      desiredWeeklyFrequency,
     })
   }
 )
