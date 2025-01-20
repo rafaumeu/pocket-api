1187a7b46902ea2a1f377bc78a141a9eff1e7d1d
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-20 07:15:58 -0300

    refactor(db): refactor database schema and seed data     - Refactored `src/db/schema.ts` to include new tables and columns.     - Refactored `src/db/seed.ts` to include new seed data.     - Updated `src/env.ts` to include new environment variables.     - Removed unnecessary files and updated migration history.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,22 @@
 import { createId } from '@paralleldrive/cuid2'
 import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 
+export const users = pgTable('users', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  name: text('name'),
+  email: text('email'),
+  avatarUrl: text('avatar_url').notNull(),
+  externalAccountId: integer('external_account_id').notNull().unique(),
+})
 export const goals = pgTable('goals', {
-  id: text('id').primaryKey().$defaultFn(() => createId()),
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  userId: text('user_id')
+    .references(() => users.id)
+    .notNull(),
   title: text('title').notNull(),
   desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
@@ -10,10 +24,13 @@ export const goals = pgTable('goals', {
     .defaultNow(),
 })
 
-
 export const goalCompletions = pgTable('goal_completions', {
-  id: text('id').primaryKey().$defaultFn(() => createId()),
-  goalId: text('goal_id').references(() => goals.id).notNull(),
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  goalId: text('goal_id')
+    .references(() => goals.id)
+    .notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
     .notNull()
     .defaultNow(),
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,18 +1,32 @@
 import dayjs from 'dayjs'
 import { client, db } from '.'
-import { goalCompletions, goals } from './schema'
+import { goalCompletions, goals, users } from './schema'
 async function seed() {
   await db.delete(goalCompletions)
   await db.delete(goals)
-  const result = await db.insert(goals).values([
-    {title: 'Acordar cedo', desiredWeeklyFrequency: 5},
-    {title: 'Fazer exercícios', desiredWeeklyFrequency: 3},
-    {title: 'Leitura', desiredWeeklyFrequency: 2},
-  ]).returning()
+  const [user] = await db
+    .insert(users)
+    .values([
+      {
+        name: 'John',
+        email: 'j@j.com',
+        avatarUrl: 'https://i.pravatar.cc/300?u=j@j.com',
+        externalAccountId: 123,
+      },
+    ])
+    .returning()
+  const result = await db
+    .insert(goals)
+    .values([
+      { userId: user.id, title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
+      { userId: user.id, title: 'Fazer exercícios', desiredWeeklyFrequency: 3 },
+      { userId: user.id, title: 'Leitura', desiredWeeklyFrequency: 2 },
+    ])
+    .returning()
   const startOfWeek = dayjs().startOf('week')
   await db.insert(goalCompletions).values([
-    {goalId: result[0].id, createdAt:  startOfWeek.toDate()},
-    {goalId: result[1].id, createdAt:  startOfWeek.add(1, 'day').toDate()},    
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
   ])
 }
 
