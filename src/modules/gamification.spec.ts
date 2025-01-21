567575276217287b50ddc9423a2c3ec0d588fedf
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-21 07:47:51 -0300

    feat(gamification): implement gamification logic and tests     - Added `src/modules/gamification.ts` with functions to calculate level from experience and experience to level.     - Added `src/modules/gamification.spec.ts` with tests for the gamification logic.

---
+++ src/modules/gamification.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from 'vitest'
+import {
+  calculateExperienceToLevel,
+  calculateLevelFromExperience,
+} from './gamification'
+
+test('experience to level', async () => {
+  const exp1 = calculateExperienceToLevel(1)
+  const exp2 = calculateExperienceToLevel(2)
+  const exp4 = calculateExperienceToLevel(4)
+  expect(exp1).toEqual(0)
+  expect(exp2).toEqual(26)
+  expect(exp4).toEqual(43)
+})
+test('level from experience', async () => {
+  const level1 = calculateLevelFromExperience(10)
+  const level2 = calculateLevelFromExperience(27)
+  const level4 = calculateLevelFromExperience(26 + 33 + 43)
+  expect(level1).toEqual(1)
+  expect(level2).toEqual(2)
+  expect(level4).toEqual(4)
+})
